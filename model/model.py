6cb9acc881f8f1dd8a7b8264183db39d15993f0c
thearyadev <personal@aryankothari.dev>
2023-08-28 02:28:25 -0400

    initial commit

---
+++ model/model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 15 * 15, 128)
+        self.fc2 = nn.Linear(128, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = x.view(-1, 16 * 15 * 15)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
