04d84efbef50e0ad1777b5c4973dfac9f4cfbeff
thearyadev <personal@aryankothari.dev>
2023-08-28 13:49:31 -0400

    add manual test cases

--- manual_validator.py
+++ manual_validator.py
@@ -2,17 +2,18 @@ from PIL import Image
 
 import torch
 from model import NeuralNetwork
-from utils.dataset_processor import TRANSFORMER
+from utils.dataset_processor import load_image
+from pathlib import Path
 
 
 model = NeuralNetwork()
-model.load_state_dict(torch.load("model.pth"))
+model.load_state_dict(torch.load("./model/model.pth"))
 model.eval()
 
-image = TRANSFORMER(Image.open("./test2.png").convert("RGB"))
+image = load_image(Path("./test4.png"))
 
 with torch.no_grad():
-    output = model(image.unsqueeze(0))
+    output = model(image)
     x, predicted = torch.max(output, 1)
 
 classnames = ["noqueue", "queue"]
