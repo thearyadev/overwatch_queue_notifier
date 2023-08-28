ed18a3af32dba5c5b1fb62846d601e5936611301
thearyadev <personal@aryankothari.dev>
2023-08-28 13:38:23 -0400

    model definition

--- model/model.py
+++ model/model.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Net(nn.Module):
+class NeuralNetwork(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(NeuralNetwork, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, 3)
         self.pool = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(16 * 15 * 15, 128)
