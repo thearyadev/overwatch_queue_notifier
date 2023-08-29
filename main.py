850373c5b8aa314a3d9be503ea9dd3cac0cb285b
thearyadev <personal@aryankothari.dev>
2023-08-29 18:14:34 -0400

    init main script for module loading via string

---
+++ main.py
@@ -0,0 +1,17 @@
+import importlib
+from utils.notify import Notify
+import argparse
+
+
+def main(notifier_module_name: str, notifier_package_name: str):
+    module = importlib.import_module(notifier_module_name)
+    notifier = getattr(module, notifier_package_name)()
+    notifier.send("Hello World")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--notifier", type=str, required=True)
+    args = parser.parse_args()
+
+    raise SystemExit(main(args.notifier.split(":")[0], args.notifier.split(":")[1]))
