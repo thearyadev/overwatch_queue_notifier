c48dc3a18f6dcd7e8408101c12ad4231a318b954
thearyadev <personal@aryankothari.dev>
2023-09-16 00:06:28 -0400

    NeuralNetwork class types

--- model/model.py
+++ model/model.py
@@ -1,18 +1,22 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 
 
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self) -> None:
         super(NeuralNetwork, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, 3)
         self.pool = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(16 * 15 * 15, 128)
         self.fc2 = nn.Linear(128, 2)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.pool(F.relu(self.conv1(x)))
         x = x.view(-1, 16 * 15 * 15)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
+
+__all__ = ["NeuralNetwork"]
